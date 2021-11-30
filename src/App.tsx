2973121ee91dbd5e268f91fb51c45986b39d455f
jpacsai <judit.pacsai@gmail.com>
2021-11-30 22:37:03 +0100

    feat: useGoogleSheets

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
-import CsvReader from './CsvReader';
+// import CsvReader from './CsvReader';
+import useGoogleSheets from 'use-google-sheets';
+
 import './App.css';
 
 function App() {
+  const { data, loading, error } = useGoogleSheets({
+    apiKey: process.env.REACT_APP_API_KEY || 'nope',
+    sheetId: process.env.REACT_APP_SHEETS_ID || 'nope',
+  });
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error!</div>;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <CsvReader />
+        {/* <CsvReader /> */}
+        {JSON.stringify(data)}
       </header>
     </div>
   );
